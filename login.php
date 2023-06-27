ae4b088c2971d0050840bcedb537c52454ba5d26
Okello Deogratius <okdeowork@gmail.com>
2023-06-27 04:45:48 +0300

    navigation bar changes

--- login.php
+++ login.php
@@ -8,8 +8,33 @@
   <link rel="stylesheet" href="bootstrap-5.3.0-dist\bootstrap-5.3.0-dist\css\bootstrap.min.css">
   <script src="bootstrap-5.3.0-dist\bootstrap-5.3.0-dist\js\bootstrap.min.js"></script>
   <title>SMW</title>
+  
 </head>
 <body>
+<div>
+  <img src="images\coverphoto.png" height="100px" width="100%" >
+</div>
+<nav class="navbar navbar-expand-lg navbar-light"style="background-color:#BF6B6B;">
+  <div class="container-fluid">
+    <a class="navbar-brand" style="color: white;" href="#">SMW</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" style="color: white;" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" style="color: white;" aria-current="page" href="aboutus.php">Aboutus</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active bgw" style="color: white;" aria-current="page" href="members.php">Members</a>
+        </li>
+       </ul>
+      </div>
+  </div>
+</nav>
 <h1 class="title">Hello WOrld</h1>
 
 </body>
